066dce002bc6af98d7de01f322dc3ecba9110ce0
skyder-dev <167886236+skyder-dev@users.noreply.github.com>
2024-06-26 02:31:31 +0200

    Add files via upload

---
+++ screen.bat
@@ -0,0 +1,6 @@
+@echo off
+color 1
+cls
+echo -----------------------------------------------L'intallation est en cours-----------------------------------------------
+python "%~dp0\screen.py"
+pause
